8f38a1c9d96f4ff87a05b8504b1268bd2037a0d9
PVG2526 <gestion.apuntes.informatica@gmail.com>
2025-12-07 23:12:09 +0100

    Errores corregidos desde vallesDev

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -64,7 +64,7 @@ namespace ejercicioTelegrama
                     coste = 0;
                 }
             }
-            txtPrecio.Text = coste.ToString() + " euross";
+            txtPrecio.Text = coste.ToString() + " euros";
         }
     }
 }
